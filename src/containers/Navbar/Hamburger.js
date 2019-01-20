8762503f276fe64a47317b13dccd87e49cab62b5
Imri Naim <imri294@gmail.com>
2019-01-20 20:43:17 +0200

    downgraded webpack, webpack-dev-serve to v3 so build will not fail

---
+++ src/containers/Navbar/Hamburger.js
@@ -0,0 +1,14 @@
+/*
+import React from 'react';
+
+const Hamburger = () => {
+  return (
+    <button className="toggle-button">
+      <span className="toggle-button__bar"/>
+      <span className="toggle-button__bar" />
+      <span className="toggle-button__bar" />
+    </button>
+  )
+};
+
+export default Hamburger;*/
